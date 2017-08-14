b9544d2009bf0fe179f9ffbbf7cdfbabf77dd2d7
Mark Vesterskov <mark@48k.dk>
2017-08-14 10:36:47 +0200

    First commit
    Signed-off-by: Mark Vesterskov <mark@48k.dk>

---
+++ src/MM_Refund_Message.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MarkVesterskov\InmobilePHPApi;
+
+class MM_Refund_Message
+{
+	private $message = '';
+	private $message_id_to_refund = '';
+	public function __construct($message, $message_id_to_refund)
+	{
+		$this->message = $message;
+		$this->message_id_to_refund = $message_id_to_refund;
+	}
+	
+	public function getMessage()
+	{
+		return $this->message;
+	}
+	
+	public function getMessageIdToRefund()
+	{
+		return $this->message_id_to_refund;
+	}
+}
